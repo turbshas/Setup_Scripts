20943a531b62f87d5c14bf2b27c95e7246f8e091
turbshas <turbshas@gmail.com>
2019-02-22 19:47:39 -0500

    Fixed pebble setup script to install actual SDKs

--- pebble_dev_setup.sh
+++ pebble_dev_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Prerequisites
-sudo apt install -y python-pip python2.7-dev python-gevent libsdl1.2debian libfdt1 libpixman-1-0 git gcc-arm-none-eabi npm perl gdb-multiarch
+sudo apt install -y python-pip python2.7-dev python-gevent libsdl1.2debian libfdt1 libpixman-1-0 git gcc-arm-none-eabi npm perl gdb-multiarch libnewlib-arm-none-eabi
 pip install virtualenv
 
 # Setup pebble-dev directory, whatever that's supposed to do
@@ -10,11 +10,14 @@ PEBBLE_DEV_TAR_NAME="$PEBBLE_DEV_NAME.tar.gz"
 if [ ! -e ~/pebble-dev ]; then
     mkdir ~/pebble-dev
     cd ~/pebble-dev
-    ~/.settings/gdown.pl https://drive.google.com/file/d/1fPYGkQJUKWAiXof1CCbCMWGZwIAELgEv/view\?usp\=sharing $PEBBLE_DEV_NAME
+    #~/.settings/gdown.pl https://drive.google.com/file/d/1fPYGkQJUKWAiXof1CCbCMWGZwIAELgEv/view\?usp\=sharing $PEBBLE_DEV_TAR_NAME
     # The name suggests it is a tar.gz file (which is actually the way I created it), but after downloading
     # from google drive, it appears to be a tar.bz2 file. idk how tf this happened, but w/e I'm too lazy to
     # change the name
-    tar -jxf $PEBBLE_DEV_NAME || exit
+
+    # Rebble finally put up a developer site, the above is no longer necessary
+    wget https://developer.rebble.io/s3.amazonaws.com/assets.getpebble.com/pebble-tool/pebble-sdk-4.5-linux64.tar.bz2
+    tar -jxf $PEBBLE_DEV_TAR_NAME || exit
     echo "export PATH=~/pebble-dev/$PEBBLE_DEV_NAME/bin:\$PATH" >> ~/.myzshrc
     . ~/.myzshrc
     cd $PEBBLE_DEV_NAME
@@ -31,11 +34,16 @@ SDK_FOLDER_NAME="pebble-sdk"
 PEBBLE_SDK="$SDK_FOLDER_NAME.tar.gz"
 if [ ! -e ~/.pebble-sdk ]; then
     cd ~
-    ~/.settings/gdown.pl https://drive.google.com/file/d/1GRXH_phqa2CNQ7BqKlLrH_cog57imJEF/view?usp=sharing $PEBBLE_SDK
+    #~/.settings/gdown.pl https://drive.google.com/file/d/1GRXH_phqa2CNQ7BqKlLrH_cog57imJEF/view?usp=sharing $PEBBLE_SDK
     # The name suggests it is a tar.gz file (which is actually the way I created it), but after downloading
     # from google drive, it appears to be a tar.bz2 file. idk how tf this happened, but w/e I'm too lazy to
     # change the name
-    tar -jxf $PEBBLE_SDK || exit
+    #tar -jxf $PEBBLE_SDK || exit
+
+    # Found a package that can be installed
+    mkdir ~/.pebble-sdk
+    touch ~/.pebble-sdk/NO_TRACKING
+    pebble sdk install https://github.com/aveao/PebbleArchive/raw/master/SDKCores/sdk-core-4.3.tar.bz2
 fi
 
 if [ ! -e ~/Pebble_Startup ]; then
